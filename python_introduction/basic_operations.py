f2f7109e0fc8aaa4c63e017718d579720780b47f
manal8654 <manalmanal2897@gmail.com>
2025-09-11 15:12:09 +0100

    fixed and tested basic_operations output

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+# Addition
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+
+# Subtraction
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+
+# Multiplication
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
