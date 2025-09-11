f952d8002e00bb7b79de2c51ec28936f57ea5e90
manal8654 <manalmanal2897@gmail.com>
2025-09-11 16:07:15 +0100

    fixed calculation formula in finance_calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,11 @@
+
 income = float(input("Enter your monthly income: "))
 expenses = float(input("Enter your total monthly expenses: "))
 
 monthly_savings = income - expenses
+
+# صيغة مفكوكة بالضبط بحال المطلوب
 annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are $" + str(int(monthly_savings)) + ".")
 print("Projected savings after one year, with interest, is: $" + str(int(annual_savings)) + ".")
-
