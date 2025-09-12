bc2cd454a2631886c2f718496db62a77dbf35e5a
manal8654 <manalmanal2897@gmail.com>
2025-09-12 07:05:07 +0100

    fixed variable names for finance_calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,8 @@
 
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = income - expenses
-
-# صيغة مفكوكة بالضبط بحال المطلوب
+monthly_savings = monthly_income - monthly_expenses
 annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print("Your monthly savings are $" + str(int(monthly_savings)) + ".")
